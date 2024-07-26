78afc6c11520ac26b77e895d3c4dbbf8434d64b3
helox <eyes22798@gmail.com>
2024-07-26 16:10:51 +0800

    chore: :hammer: add husky eslint tsconfig prettier..

---
+++ commitlint.config.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  commitlint: {
+    extends: ['@commitlint/config-conventional']
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert', 'chore']
+    ]
+  },
+  prompt: {
+    types: [
+      { name: 'feat', value: 'feat', emoji: ':sparkles:' },
+      { name: 'fix', value: 'fix', emoji: ':bug:' },
+      { name: 'docs', value: 'docs', emoji: ':memo:' },
+      { name: 'style', value: 'style', emoji: ':lipstick:' },
+      { name: 'refactor', value: 'refactor', emoji: ':recycle:' },
+      { name: 'perf', value: 'perf', emoji: ':zap:' },
+      { name: 'test', value: 'test', emoji: ':white_check_mark:' },
+      {
+        name: 'build',
+        value: 'build',
+        emoji: ':package:'
+      },
+      { name: 'ci', value: 'ci', emoji: ':ferris_wheel:' },
+      { name: 'revert', value: 'revert', emoji: ':rewind:' },
+      {
+        name: 'chore',
+        value: 'chore',
+        emoji: ':hammer:'
+      }
+    ],
+    useEmoji: true
+  }
+}
--- src/utils/flexible.ts
+++ src/utils/flexible.ts
@@ -1,11 +1,14 @@
 import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 import { PC_DEVICE_WIDTH } from '../constants'
 
 /**
  * 判断当前是否为移动设备
  */
 export const isMobileTerminal = computed(() => {
-  return document.documentElement.clientWidth < PC_DEVICE_WIDTH && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-     navigator.userAgent
+  const { width } = useWindowSize()
+  return (
+    width.value < PC_DEVICE_WIDTH &&
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
   )
 })
